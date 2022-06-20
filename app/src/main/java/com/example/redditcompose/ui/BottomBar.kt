af5e02064d1a13d3f4607dbfa0eacc9e1b0471d0
adrem7 <adrian.raisin@me.com>
2022-06-20 16:03:14 +0100

    Implemented BottomBar Issues: Discover symbol, Underline of tab indicator, Messages using Email symbol

--- app/src/main/java/com/example/redditcompose/ui/BottomBar.kt
+++ app/src/main/java/com/example/redditcompose/ui/BottomBar.kt
@@ -1,27 +1,47 @@
 package com.example.redditcompose.ui
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.height
 import androidx.compose.material.BottomAppBar
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.Icon
+import androidx.compose.material.Tab
+import androidx.compose.material.TabRow
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun BottomBar() {
     BottomAppBar(
         content = {
-            Text("This is the bottom nav bar")
-//            var tabIndex by remember { mutableStateOf(0) }
-//            val tabData = listOf(
-//                ,
-//            )
-//            TabRow(selectedTabIndex = tabIndex) {
-//                tabData.forEachIndexed { index, icon ->
-//                    Tab(selected = tabIndex == index, onClick = {
-//                        tabIndex = index
-//                    }, icon = {
-//                        Icon(imageVector = icon, contentDescription = null)
-//                    })
-//                }
-//            }
+            var tabIndex by remember { mutableStateOf(0) }
+            val tabData = listOf(
+                Icons.Filled.Home,
+                Icons.Filled.CheckCircle,
+                Icons.Filled.Add,
+                Icons.Filled.MailOutline,
+                Icons.Filled.Notifications,
+            )
+            TabRow(
+                selectedTabIndex = tabIndex,
+                indicator = {
+                    Box(modifier = Modifier.height(0.dp))
+                }
+            ) {
+                tabData.forEachIndexed { index, icon ->
+                    Tab(
+                        selected = tabIndex == index,
+                        onClick = {
+                            tabIndex = index
+                        },
+                        icon = {
+                            Icon(imageVector = icon, contentDescription = null)
+                        }
+                    )
+                }
+            }
         }
     )
 }
